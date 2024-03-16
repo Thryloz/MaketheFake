b242b0fa1c0e38df1ab5b27a72187c512783aab3
Thryloz <86812547+Thryloz@users.noreply.github.com>
2024-03-16 02:18:45 -0700

    credits partially done

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -6,18 +6,41 @@ class Credits extends Phaser.Scene{
     create() {        
         this.CREDITS = this.add.bitmapText(140, 60, 'gem', 'CREDITS', 64).setOrigin(0.5).setTint(0xffffff);
 
+        if (!this.silverwolf_gif){
+            this.silverwolf_gif = this.anims.create({
+                key: 'silverwolf_gif_animation',
+                frameRate: 24,
+                repeat: -1,
+                frames: this.anims.generateFrameNames('silverwolf_gif', {
+                    prefix: 'frame_',
+                    suffix: '_delay-0.05s',
+                    start: 0,
+                    end: 145,
+                    zeroPad: 3
+                }),
+            })
+        }
+        this.gif_base = this.add.sprite(width/2, height/2 - 10, 'silverwolf_gif', "frame_000_delay-0.05s")
+        this.gif_base.play('silverwolf_gif_animation')
+        // it took like an hour to figure out how to add a gif to phaser
+
+
         let font_size = 24;
-        this.add.bitmapText(300, 150, 'gem', 'Programming, and Art by Jim Lee', font_size).setOrigin(0.5).setTint(0xffffff);
-        this.add.bitmapText(430, 200, 'gem', 'Design and Inspiration by Hoyoverse: Honkai Star Rail', font_size).setOrigin(0.5).setTint(0xffffff);
-        this.add.bitmapText(443, 300, 'gem', 'https://www.youtube.com/watch?v=K5UL_RxK0lk', font_size).setOrigin(0.5).setTint(0xffffff);
-        this.add.bitmapText(220, 350, 'gem', 'Menu Fire sfx by leosalom', font_size).setOrigin(0.5).setTint(0xffffff);
-        this.add.bitmapText(500, 400, 'gem', 'https://freesound.org/people/leosalom/sounds/234288/', font_size).setOrigin(0.5).setTint(0xffffff);
-        this.add.bitmapText(230, 450, 'gem', 'Fire damage sfx from Mixkit', font_size).setOrigin(0.5).setTint(0xffffff);
-        this.add.bitmapText(440, 500, 'gem', 'https://mixkit.co/free-sound-effects/fire/', font_size).setOrigin(0.5).setTint(0xffffff);
-        this.add.bitmapText(210, 550, 'gem', 'All other sfx by Jim Lee', font_size).setOrigin(0.5).setTint(0xffffff);
+        this.add.bitmapText(300, 120, 'gem', 'Programming and Art by Jim Lee', font_size).setOrigin(0.5).setTint(0xffffff);
+        this.add.bitmapText(430, 170, 'gem', 'Design and Inspiration by Hoyoverse: Honkai Star Rail', font_size).setOrigin(0.5).setTint(0xffffff);
+        this.add.bitmapText(430, 550, 'gem', 'Music by', font_size).setOrigin(0.5).setTint(0xffffff);
+        this.add.bitmapText(500, 600, 'gem', 'And HUGE thanks to Nathan Altice, Nate Laffan, and Jimmy Teng\nfor teaching and to all the playtesters of CMPM120 Winter 2024', 24).setOrigin(0.5).setTint(0xffffff);
 
 
-        this.add.bitmapText(width/2, 600, 'gem', `Press ENTER to return`, 24).setOrigin(0.5).setTint(0xffffff);
+
+        this.return = this.add.bitmapText(width/2, 700, 'gem', `Press SPACE to return`, 24).setOrigin(0.5).setTint(0xffffff);
+        this.tweens.add({
+            targets: this.return,
+            duration: 800,
+            alpha: { from: 1, to: 0},
+            yoyo: true,
+            repeat: -1,
+        });
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
